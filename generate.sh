260c17ff1ae41b29580e31785a024b6958929922
Daniel Khashabi <danielk@jhu.edu>
2024-05-16 07:42:10 -0400

    update.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
-#python3 bibtex2html.py
-#python3 jemdoc.py index
-#scp index.html masters1:/users/danielk/public_html
+python3 bibtex2html.py
+python3 jemdoc.py index
+scp index.html masters1:/users/danielk/public_html
 #scp -r files/* masters1:/users/danielk/public_html
 #scp -r files/2024_quote_tuning masters1:/users/danielk/public_html/files/
 #scp -r * masters1:/users/danielk/public_html
